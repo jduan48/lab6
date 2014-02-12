dc6a4d3e53323fbec472c48814db7f221a3f06b2
Jessie Duan <jduan48@gmail.com>
2014-02-12 23:09:03 +0000

    ajax lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -26,7 +26,18 @@ function addProjectDetails(e) {
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
 
-	console.log("User clicked on project " + idNumber);
+	$.get("/project/" + idNumber, showProject);
+}
+
+function showProject(result) {
+	var contentDiv = "div#project" + result.id + " div.details";
+	
+	var img = "<img src='" + result.image + "' class='detailsImage'>";
+	var title = "<h3>" + result.title + "</h3>";
+	var date = "<p>" + result.date + "</p>";
+	var desc = result.summary;
+
+	$(contentDiv).html(img + title + date + desc);
 }
 
 /*
@@ -34,5 +45,14 @@ function addProjectDetails(e) {
  * and apply it
  */
 function randomizeColors(e) {
-	console.log("User clicked on color button");
+	$.get("/palette", changeColors);
+}
+
+function changeColors(result) {
+	var colors = result.colors.hex;
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);
 }
